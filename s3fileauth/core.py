2b1b09d86597bd1a4257de332255a9d61192a825
Nick Humrich <nick.humrich@gmail.com>
2018-03-23 11:21:18 -0600

    Add heartbeat

--- s3fileauth/core.py
+++ s3fileauth/core.py
@@ -62,11 +62,17 @@ async def stream_file(request):
 async def handle_auth(request):
     return web.Response(status=204)
 
+async def heartbeat_middleware_factory(app, handler):
+    async def heartbeat(request):
+        if request.path == '/healthz'
+            return web.Response(status=200)
+        return await handler(request)
+    return heatbeat
 
 def init():
     global session
     session = aiobotocore.get_session()
-    app = web.Application()
+    app = web.Application(middlewares=heartbeat_middleware_factory)
     aiogithubauth.add_github_auth_middleware(
         app,
         github_id=gh_id,
